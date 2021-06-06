20df38d9992112d07b7b88f3ee80d2e715a0575c
Thomas Kim <thomaskim@Thomass-Mac-mini-2.local>
2021-06-06 06:50:30 -0400

    create accountsettings component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar/Navbar"
 import Home from "./components/Links/Home/Homepage"
 import Publish from "./components/Links/Publish/Publish"
+import AccountSettings from "./components/AccountSettings/AccountSettings"
 import PostShow from "./components/Show/Show"
 
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Publish />
+      <AccountSettings />
     </div>
   );
 }
---
+++ src/components/AccountSettings/AccountSettings.jsx
@@ -0,0 +1,9 @@
+import "./accountsettings.css"
+
+export default function AccountSettings() {
+  return (
+    <div className="accountSettings">
+      
+    </div>
+  )
+}
