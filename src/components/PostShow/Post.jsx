bcda826553ba46ea2333f49c5c44e7c6106e2c79
Thomas Kim <thomaskim@Thomass-Mac-mini-2.local>
2021-06-06 04:35:28 -0400

    Add styling to postShow

--- src/components/PostShow/Post.jsx
+++ src/components/PostShow/Post.jsx
@@ -2,7 +2,7 @@ import "./post.css"
 
 export default function Post() {
   return (
-    <div >
+    <div className="post" >
       <img 
       className="postImg" src="https://www.chf.or.kr/jnrepo/namo/img/images/000005/20210119182559666_QHTTNT8F.jpg" alt="Korean traditional food" />
       <div className="postInfo">
@@ -16,6 +16,7 @@ export default function Post() {
         <hr/>
         <span className="postDate">1 hour ago</span>
       </div>
+      <p className="postDesc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, quasi earum! Iure labore nobis eos aliquid maiores quidem quasi corporis debitis omnis. Libero consequuntur aspernatur quae quaerat praesentium, ratione iusto.</p>
     </div>
   )
 }
