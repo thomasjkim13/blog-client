2b7b177075de5462fecc3893318bbef56c27f1e7
Thomas Kim <thomaskim@Thomass-Mac-mini-2.local>
2021-06-06 07:18:40 -0400

    Update styling and create signin component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar/Navbar"
 import Home from "./components/Links/Home/Homepage"
 import Publish from "./components/Links/Publish/Publish"
 import AccountSettings from "./components/AccountSettings/AccountSettings"
+import SignIn from "./components/SignIn/SignIn"
 import PostShow from "./components/Show/Show"
 
 
@@ -9,7 +10,7 @@ function App() {
   return (
     <div>
       <Navbar />
-      <AccountSettings />
+      <SignIn />
     </div>
   );
 }
---
+++ src/components/SignIn/SignIn.jsx
@@ -0,0 +1,9 @@
+import "./signin.css"
+
+export default function SignIn() {
+  return (
+    <div className="signin">
+      signin
+    </div>
+  )
+}
