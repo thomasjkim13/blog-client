994d8fb3e757ea12382c6226d3eb847034d39e2b
Thomas Kim <thomaskim@Thomass-Mac-mini-2.local>
2021-06-05 22:39:25 -0400

    Add image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,9 +5,9 @@ export default function Header() {
     <div className="header">
       <div className="headerTitle">
         <span className="headerTitleSm">See The World</span>
-        <span className="headerTitleSm">TK Blog</span>
+        <span className="headerTitleLg">TK Blog</span>
       </div>
-      <img className="headerImg" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/travel-1585330690.jpg" alt="" />
+      <img className="headerImg" src="https://www.workaway.info/gfx/2015/content/frontpage/header_frontpage_2.jpg" alt="" />
     </div>
   )
 }
