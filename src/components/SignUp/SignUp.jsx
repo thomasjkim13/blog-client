a52ca2f24c34d02d0ef2393dde8c2f1ea720a1a2
Thomas Kim <thomaskim@Thomass-Mac-mini-2.local>
2021-06-07 02:32:48 -0400

    axios call for signup

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,18 +1,48 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import axios from "axios"
 import "./signup.css"
 
 export default function SignUp() {
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const res = await axios.post("/auth/register", {
+      username, 
+      email, 
+      password
+    })
+    console.log(res)
+  }
   return (
     <div className="signup">
       <span className="signupTitle">SignUp</span>
-      <form className="signupForm">
+      <form className="signupForm" onSubmit={ handleSubmit }>
         <label>Username</label>
-        <input type="text" className="signupInput" placeholder="Enter your username..." />
+        <input 
+          type="text" 
+          className="signupInput" 
+          placeholder="Enter your username..." 
+          onChange={e => setUsername(e.target.value)}
+        />
         <label>Email</label>
-        <input type="text" className="signupInput" placeholder="Enter your email..." />
+        <input 
+          type="text" 
+          className="signupInput" 
+          placeholder="Enter your email..." 
+          onChange={e => setEmail(e.target.value)}
+        />
         <label>Password</label>
-        <input type="password" className="signupInput" placeholder="Enter your password..." />
-        <button className="signupBtn">SignUp</button>
+        <input 
+          type="password" 
+          className="signupInput" 
+          placeholder="Enter your password..." 
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button className="signupBtn" type="submit">SignUp</button>
       </form>
       <button className="signupRegisterBtn">
         <Link className="link" to="/signin">SignIn</Link>
