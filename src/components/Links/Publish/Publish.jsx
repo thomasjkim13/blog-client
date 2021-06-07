43829aa0884ec686870cd1d367d2a17475abb02f
Thomas Kim <thomaskim@Thomass-Mac-mini-2.local>
2021-06-07 05:34:05 -0400

    upload file

--- src/components/Links/Publish/Publish.jsx
+++ src/components/Links/Publish/Publish.jsx
@@ -1,26 +1,59 @@
+import { useState, useContext } from "react"
 import "./publish.css"
+import axios from "axios"
+import { Context } from "../../../context/Context"
 
 export default function Publish() {
+  const [title, setTitle] = useState("")
+  const [desc, setDesc] = useState("")
+  const [file, setFile] = useState(null)
+  const {user} = useContext(Context)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const newPost = {
+      username:user.username,
+      title,
+      desc
+    }
+    if(file) {
+      const data = FormData()
+      const filename = Date.now() + file.name
+      data.append("name", filename)
+      data.append("file", file)
+      newPost.photo = filename;
+      try {
+        await axios.post("/upload", data) 
+      } catch (err) {}
+    }
+    try {
+      const res = await axios.post("/posts", newPost)
+      window.location.replace("/post/" + res.data._id)
+    } catch (err) {}
+  }
+
   return (
     <div className="publish">
-      <img className="publishImg"
-        src="https://www.chf.or.kr/jnrepo/namo/img/images/000005/20210119182559666_QHTTNT8F.jpg"
-        alt=""
-      />
-      <form className="publishForm">
+      {file && (
+        <img className="publishImg"
+          src={URL.createObjectURL(file)}
+          alt=""
+        />
+      )}
+      <form className="publishForm" onSubmit = {handleSubmit}>
         <div className="publishFormGroup">
           {/* links to id="fileInput" */}
           <label htmlFor="fileInput">
             <i className="publishIcon fas fa-folder-plus"></i>
           </label>
-          <input type="file" id="fileInput" style={{display:"none"}} />
+          <input type="file" id="fileInput" style={{display:"none"}} onChange={(e) => setFile(e.target.files[0])}/>
           {/* autoFocus automatically focuses on the text input */}
           <input type="text" placeholder="Title" className="publishInput" autoFocus={true} />
         </div>
         <div className="publishFormGroup">
           <textarea placeholder="Tell your story..." type="text" className="publishInput publishText"></textarea>
         </div>
-        <button className="publishBtn">Publish</button>
+        <button className="publishBtn" type="submit">Publish</button>
       </form>
     </div>
   )
